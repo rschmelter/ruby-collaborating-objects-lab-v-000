1205473388768d2eae85d88d7b638a440a885c5a
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-19 17:48:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,14 +14,14 @@ class Song
   #   end
   # end
 
-  # def self.new_by_filename(file_name)
-  #   file = file_name.split(" - ")
-  #   artist = file[0]
-  #   song = file[1]
-  #   new_song = self.new(song)
-  #   new_song.artist.name = artist
-  #
-  # end
+  def self.new_by_filename(file_name)
+    file = file_name.split(" - ")
+    artist = file[0]
+    song = file[1]
+    new_song = self.new(song)
+    new_song.artist.name = artist
+
+  end
 
 
 end
