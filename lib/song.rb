84f8e99ff1d37e2ce8491326e9ecb1e8b6ad71b7
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-19 17:39:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   def initialize(name)
     @name = name
   end
-  #
+  
   # def artist=(name)
   #   if self.artist.nil?
   #     artist.name = Artist.new(name)
@@ -13,15 +13,15 @@ class Song
   #     self.artist.name = name
   #   end
   # end
-  #
-  # def self.new_by_filename(file_name)
-  #   file = file_name.split(" - ")
-  #   artist = file[0]
-  #   song = file[1]
-  #   new_song = self.new(song)
-  #   new_song.artist.name = artist
-  #
-  # end
+  
+  def self.new_by_filename(file_name)
+    file = file_name.split(" - ")
+    artist = file[0]
+    song = file[1]
+    new_song = self.new(song)
+    new_song.artist.name = artist
+  
+  end
 
 
 end
