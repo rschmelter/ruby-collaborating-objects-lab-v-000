462cae35760883a4dc476d4f6e6290385e32a844
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-19 17:32:01 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+
+
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def self.find_or_create_by_name(artist)
+    artist_instance = @@all.detect {|a| a.name == artist}
+    if artist_instance
+      artist_instance
+  else
+      artist = Artist.new(artist)
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+  #
+  # def artist=(name)
+  #   if self.artist.nil?
+  #     artist.name = Artist.new(name)
+  #   else
+  #     self.artist.name = name
+  #   end
+  # end
+  #
+  # def self.new_by_filename(file_name)
+  #   file = file_name.split(" - ")
+  #   artist = file[0]
+  #   song = file[1]
+  #   new_song = self.new(song)
+  #   new_song.artist.name = artist
+  #
+  # end
+
+
+end
